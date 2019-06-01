84b7d7a87b0c1fe0453b589a02d26863f0b17331
bigsan33 <yuichi887+sub@gmail.com>
2019-06-01 21:34:42 +0900

    Lesson7の課題を提出します。

---
+++ Assets/BlockSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSound : MonoBehaviour {
+
+	public AudioClip sound;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//衝突時に呼ばれる関数
+	void OnCollisionEnter2D (Collision2D collision){
+
+		if (collision.gameObject.tag == "block") {
+			AudioSource.PlayClipAtPoint (sound, transform.position);
+		
+		} else if (collision.gameObject.tag == "ground") {
+			AudioSource.PlayClipAtPoint (sound, transform.position);
+		}
+
+	}
+}
